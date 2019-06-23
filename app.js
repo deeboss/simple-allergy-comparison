6d71ac7f0d2a18c7edb992bd9b9abc0429aac308
Don <hello@donbos.co>
2019-06-23 17:14:29 +0800

    client side can add more products for comparison

--- app.js
+++ app.js
@@ -3,19 +3,19 @@
 // - Ingredients
 
 let products = {
-	"colgate": {
+	"product1": {
 		"name": "Colgate Optic White - Sparkling Mint",
 		"ingredients": "Sodium monofluorophosphate 0.76% (0.15% w/v fluoride ion), calcium pyrophosphate, propylene glycol, PEG/PPG 116/66 copolymer, PEG-12, glycerin, PVP, flavor, sodium lauryl sulfate, tetrasodium pyrophosphate, silica, hydrogen peroxide, sodium saccharin, phosphoric acid, sucralose, butylated hydroxytoluene, water"
 	},
-	"crest": {
+	"product2": {
 		"name": "Crest",
-		"ingredients": "Aqua, Sorbitol, Hydrated Silica, PEG-6, Sodium Lauryl Sulfate, Tetrapotassium Pyrophosphate, Disodium Pyrophosphate, Disodium Pyrophosphate, Tetrasodium Pyrophosphate, Aroma, Cellulose Gum, Xanthan Gum, Carbomer, Sodium Saccharin, Triclosan, Copernicia Cerifera (Carnauba) Wax, CI 77891, Limonene, CI 42090, Sodium Fluoride (0.321%)"
+		"ingredients": "Aqua, Sorbitol, Hydrated Silica, PEG-6, Sodium Lauryl Sulfate, Tetrapotassium Pyrophosphate, Disodium Pyrophosphate, Tetrasodium Pyrophosphate, Aroma, Cellulose Gum, Xanthan Gum, Carbomer, Sodium Saccharin, Triclosan, Copernicia Cerifera (Carnauba) Wax, CI 77891, Limonene, CI 42090, Sodium Fluoride (0.321%)"
 	},
-	"sensodyne": {
+	"product3": {
 		"name": "Sensodyne",
 		"ingredients": "Potassium nitrate, sodium fluoride, Water, hydrated silica, sorbitol, glycerin, pentasodium triphosphate, PEG-8, flavor, titanium dioxide, sodium methyl cocoyl taurate, cocamidopropyl betaine, xanthan gum, sodium hydroxide, sodium saccharin, sucralose"
 	},
-	"darlie": {
+	"product4": {
 		"name": "Darlie Double Action Toothpaste",
 		"ingredients": "Dicalcium Phosphate Dihydrate, Water, Sorbitol, Glycerin, Dicalcium Phosphate, Sodium Lauryl Sulphate, Flavour (Contains Spearmint Essence & Peppermint Essence), Carageenan, Sodium Lauroyl Sarcosinate, Sodium Monofluorophosphate, Tetrasodium Pyrophosphate, Sodium Saccharin"
 	}
@@ -34,64 +34,147 @@ let products = {
 
 let numOfProducts = Object.keys(products).length;
 let arr = Object.keys(products);
-let allIngredientsArr = [];
-// Capitalize string for better formatting and consistency
-String.prototype.capitalize = function() {
-	return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-};
 
-for (let idx = 0; idx < numOfProducts; ++idx) {
-	let id = arr[idx];
-	let tmpArr = products[id].ingredients.split(', ');
-	let ingredientsArr = [];
-	products[id].ingredients = tmpArr.sort();
+
+// Begin
+let findDuplicateIngredients = () => {
+	let allIngredientsArr = [];
+	// Capitalize string for better formatting and consistency
+	String.prototype.capitalize = function() {
+		return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+	};
 	
-	for (let i = 0; i < products[arr[idx]].ingredients.length; ++i) {
-		ingredientsArr.push(products[id].ingredients[i].capitalize());
-		allIngredientsArr.push(products[id].ingredients[i].capitalize());
+	// Trim whitespace
+	ltrim = (str) => {
+			if(str == null) return str;
+			return str.replace(/^\s+/g, '');
 	}
-	products[id].ingredients = ingredientsArr.sort();
+
+	for (let idx = 0; idx < numOfProducts; ++idx) {
+		let id = arr[idx];
+		// console.log(products[id].ingredients);
+		// ISSUE: need to ensure compatibiltiy for items without a space
+		let tmpArr = products[id].ingredients.toString().split(',');
+		let ingredientsArr = [];
+		products[id].ingredients = tmpArr.sort();
+
+		for (let i = 0; i < products[id].ingredients.length; ++i) {
+			ingredientsArr.push(ltrim(products[id].ingredients[i].capitalize()));
+			allIngredientsArr.push(ltrim(products[id].ingredients[i].capitalize()));
+		}
+		products[id].ingredients = ingredientsArr.sort();
+	}
+	
+	const count = allIngredientsArr => allIngredientsArr.reduce((a, b) => ({ ...a,
+																																						[b]: (a[b] || 0) + 1
+																																					}), {}) // initialize the accumulator
+
+	// Set the number of duplicates across the products
+	let duplicateThreshold = numOfProducts - 1;
+
+	const duplicates = dict =>
+	Object.keys(dict).filter((a) => dict[a] > duplicateThreshold)
+
+	let listOfDuplicates = count(allIngredientsArr)
+	let results = duplicates(count(allIngredientsArr))
+	numOfDuplicates = results.length;
+	
+		// 		Find them in each product, and re-order their respective ingredients array
+		// 		to make sure they're first.
+		let array_move = (arr, old_index, new_index) => {
+			if (new_index >= arr.length) {
+				var k = new_index - arr.length + 1;
+				while (k--) {
+					arr.push(undefined);
+				}
+			}
+			arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+			return arr; // for testing
+		};
+
+		for (let i = 0; i < numOfProducts; ++i) {
+			let id = arr[i];
+			for (let j = 0; j < numOfDuplicates; ++j) {
+				let indexOfDuplicate = products[arr[i]].ingredients.indexOf(results[j]);
+				let sortedArr = array_move(products[arr[i]].ingredients, indexOfDuplicate, j)
+				console.log(sortedArr);
+				// products[id].ingredients = sortedArr;
+			}
+		}
+	
 }
 
-const count = allIngredientsArr =>
-  allIngredientsArr.reduce((a, b) => ({ ...a,
-    [b]: (a[b] || 0) + 1
-  }), {}) // don't forget to initialize the accumulator
+// End
 
-// Set the number of duplicates across the products
-let duplicateThreshold = numOfProducts - 1;
 
-const duplicates = dict =>
-  Object.keys(dict).filter((a) => dict[a] > duplicateThreshold)
 
-let listOfDuplicates = count(allIngredientsArr)
-let results = duplicates(count(allIngredientsArr))
-let numOfDuplicates = results.length;
 
-// 		Find them in each product, and re-order their respective ingredients array
-// 		to make sure they're first.
-function array_move(arr, old_index, new_index) {
-    if (new_index >= arr.length) {
-        var k = new_index - arr.length + 1;
-        while (k--) {
-            arr.push(undefined);
-        }
-    }
-    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-    return arr; // for testing
-};
 
-// Array, from, to
-// console.log(array_move([1, 2, 3], 2, 0)); 
 
-for (let i = 0; i < numOfProducts; ++i) {
-	let id = arr[i];
-	for (let j = 0; j < numOfDuplicates; ++j) {
-		let indexOfDuplicate = products[arr[i]].ingredients.indexOf(results[j]);
-		let sortedArr = array_move(products[arr[i]].ingredients, indexOfDuplicate, j)
-		products[id].ingredients = sortedArr;
-	}
-}
+
+
+
+
+
+
+// let allIngredientsArr = [];
+// // Capitalize string for better formatting and consistency
+// String.prototype.capitalize = function() {
+// 	return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+// };
+
+// for (let idx = 0; idx < numOfProducts; ++idx) {
+// 	let id = arr[idx];
+// 	// ISSUE: need to ensure compatibiltiy for items without a space
+// 	let tmpArr = products[id].ingredients.split(',');
+// 	let ingredientsArr = [];
+// 	products[id].ingredients = tmpArr.sort();
+
+// 	for (let i = 0; i < products[id].ingredients.length; ++i) {
+// 		ingredientsArr.push(products[id].ingredients[i].capitalize());
+// 		allIngredientsArr.push(products[id].ingredients[i].capitalize());
+// 	}
+// 	products[id].ingredients = ingredientsArr.sort();
+// }
+
+// const count = allIngredientsArr =>
+// allIngredientsArr.reduce((a, b) => ({ ...a,
+// 																																					[b]: (a[b] || 0) + 1
+// 																																				}), {}) // don't forget to initialize the accumulator
+
+// // Set the number of duplicates across the products
+// let duplicateThreshold = numOfProducts - 1;
+
+// const duplicates = dict =>
+// Object.keys(dict).filter((a) => dict[a] > duplicateThreshold)
+
+// let listOfDuplicates = count(allIngredientsArr)
+// let results = duplicates(count(allIngredientsArr))
+// let numOfDuplicates = results.length;
+
+// // 		Find them in each product, and re-order their respective ingredients array
+// // 		to make sure they're first.
+// let array_move = (arr, old_index, new_index) => {
+// 	if (new_index >= arr.length) {
+// 		var k = new_index - arr.length + 1;
+// 		while (k--) {
+// 			arr.push(undefined);
+// 		}
+// 	}
+// 	arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+// 	return arr; // for testing
+// };
+
+// for (let i = 0; i < numOfProducts; ++i) {
+// 	let id = arr[i];
+// 	for (let j = 0; j < numOfDuplicates; ++j) {
+// 		let indexOfDuplicate = products[arr[i]].ingredients.indexOf(results[j]);
+// 		let sortedArr = array_move(products[arr[i]].ingredients, indexOfDuplicate, j)
+// 		products[id].ingredients = sortedArr;
+// 	}
+// }
+
+
 
 // 3. For each toothpaste, create a new row for each ingredient
 // 					Every entry has a <tr></tr>
@@ -103,8 +186,10 @@ for (let i = 0; i < numOfProducts; ++i) {
 let createTableHeader = () => {
 	let table = document.getElementById('result').getElementsByTagName('thead')[0];
 	let row = table.insertRow();
+//	console.log("products to render: " + numOfProducts);
 	
 	for (let idx = 0; idx < numOfProducts; ++idx) {
+
 		let header = document.createElement('th');
 		header.innerText = products[arr[idx]].name;
 		row.append(header);
@@ -142,17 +227,69 @@ let createTableRow = () => {
 				row.append(data);
 			}
 		}
-}
 	
+		// Client side: show matches
+		for (let i = 0; i < numOfDuplicates; ++i) {
+			let matchesEl = document.querySelectorAll("tbody tr");
+			matchesEl[i].classList.add("match")
+		}
+}
+
+let clearTable = () => {
+		let tableHeader = document.getElementById('result').getElementsByTagName('thead')[0];
+		let tableBody = document.getElementById('result').getElementsByTagName('tbody')[0];
+	
+		while (tableHeader.firstChild) {
+			tableHeader.removeChild(tableHeader.firstChild);
+		}
+	
+	while (tableBody.firstChild) {
+			tableBody.removeChild(tableBody.firstChild);
+		}
+}
+
+// Refresh program/functions, with the refreshed products as the input
+let addNewProduct = (res) => {
+	numOfProducts = ++numOfProducts;
+	let key = "product" + numOfProducts;
+	arr.push(key)
+	products[key] = res;
+
+	clearTable();
+	findDuplicateIngredients();
+	createTableHeader();
+	createTableRow();
+}
+
+
+// Client side: create table
+findDuplicateIngredients();
 createTableHeader();
 createTableRow();
 
+// Client side: Add new product for comparison
+// Client enters product name, ingredients, and presses submit
+// All fields mandatory
+// Save as object with key: productNth
+// Push to products Obj
 
-
-// For Styling Purposes
-
-for (let i = 0; i < numOfDuplicates; ++i) {
-	let matchesEl = document.querySelectorAll("tbody tr");
-	matchesEl[i].classList.add("match")
+document.getElementById("addProduct").onclick = () => {
+		let newProductName = document.getElementById("newProductName");
+		let newIngredient = document.getElementById("newIngredient");
+  if (newProductName.value === '') {
+			alert("Required field empty: A product name is needed!");
+		} else if (newIngredient.value === '') {
+			alert("Required field empty: Ingredients are needed!");
+		} else {
+			let newProduct = {
+				"name": newProductName.value,
+				"ingredients": [newIngredient.value]
+			}
+			
+			newProductName.value = '';
+			newIngredient.value = '';
+			
+			addNewProduct(newProduct);
+		}
 }
 
